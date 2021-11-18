6571d51d78a4ac90eca84374c1d9cd55a753854e
JakobusV <51762364+JakobusV@users.noreply.github.com>
2021-11-17 19:25:19 -0700

    +Connection.js

---
+++ Backend/connection.js
@@ -0,0 +1,30 @@
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+    authentication: {
+        options: {
+            userName: "Madmin", 
+            password: "JJCZ2023-" 
+        },
+        type: "default"
+    },
+    server: "pro150server.database.windows.net", 
+    options: {
+        database: "MadLibs", 
+        encrypt: true
+    }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log("Working!");
+    }
+});
+
+connection.connect();
