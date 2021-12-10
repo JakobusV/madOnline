712b4803192b29b8a924695bdb7f9be093f6b3ea
ziggityofficial <60523976+ziggityofficial@users.noreply.github.com>
2021-12-09 17:04:18 -0700

    zack stuff

--- Backend/public/populate.js
+++ Backend/public/populate.js
@@ -2,7 +2,8 @@ const starName = document.getElementById('starName');
 const proof = document.getElementById('proof');
 
 const gatherProfile = async () => {
-    const response = await fetch("http://localhost:8085/hello");
+    // const response = await fetch("http://localhost:8085/hello");
+    const response = await fetch("https://madstar.herokuapp.com/hello");
     const data = await response.json();
     console.log(data);
     // const response = await fetch("https://swapi.dev/api/people/" + Math.floor(Math.random() * 80));
